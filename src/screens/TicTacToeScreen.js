eb32c1e40d2a8d4da2b4aea6d9dcdad133b0a4fc
jiejunguo <jiejunguo@gmail.com>
2022-01-10 13:40:38 -0800

    Create circle and cross

--- src/screens/TicTacToeScreen.js
+++ src/screens/TicTacToeScreen.js
@@ -5,9 +5,15 @@ import bg from "../../assets/image/bg.jpeg";
 const TicTacToeScreen = () => {
   return (
     <View style={styles.container}>
+      {/* https://mehrankhandev.medium.com/understanding-resizemode-in-react-native-dd0e455ce63 */}
       <ImageBackground source={bg} style={styles.bg}>
-        <Text>TicTacToeScreen</Text>
+        <View style={styles.circle} />
+        <View>
+          <View style={styles.crossLine} />
+          <View style={[styles.crossLine, styles.crossLineReversed]} />
+        </View>
       </ImageBackground>
+      <StatusBar style="light" />
     </View>
   );
 };
@@ -16,27 +22,46 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#242034",
   },
   bg: {
     width: "100%",
     height: "100%",
-  },
-  diceContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    marginTop: 80,
-  },
-  button: {
-    backgroundColor: "#eae2b7",
-    height: 90,
-    justifyContent: "center",
     alignItems: "center",
-    margin: 25,
-    borderRadius: 10,
+    justifyContent: "center",
+    paddingTop: 22,
   },
-  buttonText: {
-    fontSize: 30,
-    color: "white",
+  circle: {
+    width: 85,
+    height: 85,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10,
+    borderWidth: 10,
+    borderColor: "white",
+  },
+
+  crossLine: {
+    position: "absolute",
+    width: 15,
+    height: 95,
+    backgroundColor: "white",
+    borderRadius: 5,
+    transform: [
+      {
+        rotate: "45deg",
+      },
+    ],
+  },
+  crossLineReversed: {
+    transform: [
+      {
+        rotate: "-45deg",
+      },
+    ],
   },
 });
 
